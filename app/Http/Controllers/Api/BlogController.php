84881fb273d98a14f0a3b5036c846b8826841607
RafaEscobar <rafael.eg.code@gmail.com>
2023-03-26 16:43:59 -0600

    Fin

---
+++ app/Http/Controllers/Api/BlogController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class BlogController extends Controller
+{
+    public function index(){
+        $blog = Blog::all();
+
+        $aqui = response()->json([
+            "resultado" => $blog
+        ], Response::HTTP_OK);
+
+
+
+        return view('welcome', compact('blog'));
+    }
+
+    public function store(Request $request){
+        $blog = new Blog();
+
+        $blog->nombre = $request->nombre;
+        $blog->descripcion = $request->descripcion;
+        $blog->img = $request->img;
+        $blog->save();
+
+        return response()->json(['Resultado' => $blog], Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id){
+        $blog = Blog::findOrFail($request->id);
+
+        $blog->nombre = $request->nombre;
+        $blog->descripcion = $request->descripcion;
+        $blog->img = $request->img;
+        $blog->save();
+
+        return response()->json(['Resultado' => $blog], Response::HTTP_OK);
+    }
+
+    public function destroy($id){
+        Blog::destroy($id);
+        return response()->json(['Mensaje' => 'Registro eliminado'], Response::HTTP_OK);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(BlogController::class)->group(function(){
+    Route::get('/blogs', 'index');
+    Route::post('/store', 'store');
+    Route::put('/update/{id}', 'update');
+    Route::delete('/destroy/{id}', 'destroy');
 });
